9a8c57694fb1e1a39e0f00a6d08566b77239fcdd
Dakosiu <82579141+Dakosiu@users.noreply.github.com>
2023-06-05 15:42:35 +0200

    syf

--- othire/data/actions/scripts/drzwi.lua
+++
@@ -1,9 +0,0 @@
-function onUse(cid, item)
-if getPlayerStorageValue(cid, 343455) == 1 then
-doTeleportThing(cid, {x=996,y=1004,z=7})
-else
-doPlayerSendCancel(cid, "nie masz dostepu.")
-end
-return true
-end
-    
--- othire/data/actions/scripts/nowyshop.lua
+++
@@ -1,9 +0,0 @@
-function onUse(cid, item)
-if getPlayerStorageValue(cid, 343450) == 1 then
-doTeleportThing(cid, {x=666,y=1040,z=6})
-else
-doPlayerSendCancel(cid, "nie masz dostepu.")
-end
-return true
-end
-    
