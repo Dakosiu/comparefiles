eee0302dda2482b2f0a98d49253e1e047b04266e
Dakosiu <82579141+Dakosiu@users.noreply.github.com>
2024-07-20 21:50:25 +0200

    clean

--- data-canary/scripts/test.lua
+++
@@ -1,8 +0,0 @@
--- local bless = TalkAction("!testo")
-
--- function bless.onSay(player, words, param)
-	-- player:say("Check store boost")
-	-- player:setGrindingXpBoost(10)
--- end
-
--- bless:register()
--- data/chatchannels/scripts/dungeon.lua
+++
@@ -1,7 +0,0 @@
-function canJoin(player)
-	return player:getStorageValue(dungeonStorages.inDungeon) ~= -1
-end
-
-function onSpeak(player, type, message)
-	return false
-end
